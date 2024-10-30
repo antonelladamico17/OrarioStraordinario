6e235ca1cc1efcfb4718e90d8928e1d60064f674
Antonella D'Amico <60288164+antonelladamico17@users.noreply.github.com>
2024-10-30 17:33:31 +0100

    Update calcolo.py

--- calcolo.py
+++ calcolo.py
@@ -29,9 +29,12 @@ def main():
         df['Ore_straordinarie'] = df['Differenza'].where(df['Differenza'] > orario_lavorativo_standard, pd.Timedelta(0)) - orario_lavorativo_standard
         df['Ore_straordinarie'] = df['Ore_straordinarie'].where(df['Ore_straordinarie'] > pd.Timedelta(0), pd.Timedelta(0))
 
+        # Formattazione delle ore straordinarie come stringhe
+        df['Ore_straordinarie'] = df['Ore_straordinarie'].apply(format_timedelta)
+
         # Creazione del riepilogo mensile
         df['Mese_Anno'] = df['Uscita'].dt.to_period('M')
-        riepilogo = df.groupby('Mese_Anno')['Ore_straordinarie'].sum().reset_index()
+        riepilogo = df.groupby('Mese_Anno')['Ore_straordinarie'].first().reset_index()
 
         st.write("Riepilogo delle Ore Straordinarie:")
         st.dataframe(riepilogo)
@@ -39,6 +42,21 @@ def main():
         # Opzione per scaricare il riepilogo in formato Excel
         excel_file = create_excel_file(riepilogo)
         st.download_button(label="Scarica Riepilogo", data=excel_file, file_name='riepilogo_ore_straordinarie.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+def format_timedelta(td):
+    if td == pd.Timedelta(0):
+        return "0:00:00"
+    
+    total_seconds = int(td.total_seconds())
+    days, remainder = divmod(total_seconds, 86400)  # 86400 secondi in un giorno
+    hours, remainder = divmod(remainder, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    
+    # Se i giorni sono maggiori di 0, aggiungi 24 alle ore
+    if days > 0:
+        hours += days * 24
+    
+    return f"{hours}:{minutes:02}:{seconds:02}"
 
 def create_excel_file(df):
     # Crea un file Excel in memoria
